71cb168e80444d4f81929bb9c4f86dfaf77c488c
noda3 <noda3jp@yahoo.co.jp>
2021-11-01 07:04:11 +0900

    Create lic_file_make.py

---
+++ lic_file_make.py
@@ -0,0 +1,8 @@
+import json
+
+# pip-licenses --with-urls --with-authors --format=json --with-license-file --filter-strings --output-file lics.json
+
+json_open = open('lic', 'r')
+json_load = json.load(json_open)
+pass
+
